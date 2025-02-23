33f422445deecc41adfb2887c00c4f20245a8077
Valzyy <jkt48connect@gmail.com>
2025-02-23 22:06:19 +0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     try {
       const { data: fileData } = await axios.get(
         `https://api.github.com/repos/${GITHUB_USERNAME}/${GITHUB_REPO}/contents/${filePath}`,
-        { headers: { Authorization: `token ${GITHUB_TOKEN}` } }
+        { headers: { Authorization: `Bearer ${GITHUB_TOKEN.trim()}` } }
       );
       sha = fileData.sha;
     } catch (error) {
